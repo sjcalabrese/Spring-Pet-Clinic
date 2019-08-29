b934705c6105072494e2527459223b35b230ae27
sjcalabrese <erotavlas103@gmail.com>
2019-08-29 09:02:01 -0400

    Implemented Map Imple for Visit Service. closes #47

---
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.springpetclinic.services;
+
+import guru.springframework.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
