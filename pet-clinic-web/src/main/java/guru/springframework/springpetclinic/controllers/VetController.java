5d18e7ed35b918b795db602e3f09fc8d3f1cf1fa
Sal Calabrese <erotavlas103@gmail.com>
2023-03-23 09:44:19 -0400

    Implement vets json response. closes #69

--- pet-clinic-web/src/main/java/guru/springframework/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/springpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package guru.springframework.springpetclinic.controllers;
 
+import guru.springframework.springpetclinic.model.Vet;
 import guru.springframework.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -14,11 +18,17 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html", "vets.html"})
+    @GetMapping(value = {"/vets", "/vets/index", "/vets/index.html", "vets.html"})
     public String listVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping(value = {"/api/vets"})
+    public @ResponseBody Set<Vet> getVetsJson(){
+        //@ResponseBody will default to json
+        return vetService.findAll();
     }
 }
